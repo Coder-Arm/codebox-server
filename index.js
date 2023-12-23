7ae3fc2e281a69e700aaeda2453f1894fdef4561
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-12-23 12:24:18 +0530

    changes

--- index.js
+++ index.js
@@ -14,7 +14,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
-app.use(cors());
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 require('dotenv').config();
 const PORT = process.env.PORT;
