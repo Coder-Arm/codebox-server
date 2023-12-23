b4d036d32f25c015a2b2d3b288c4e11b8c3c9381
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-12-23 13:23:08 +0530

    again

--- index.js
+++ index.js
@@ -14,12 +14,15 @@ app.use(express.json());
 app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
-const corsOptions ={
-    origin:'http://localhost:3000', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+app.use(cors())
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', true);
+    next();
+  });
 
 require('dotenv').config();
 const PORT = process.env.PORT;
