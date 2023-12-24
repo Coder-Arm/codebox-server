aa5da39abc763a840bb17c16dbc2ae6cfa282558
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-12-24 13:17:43 +0530

    removed

--- index.js
+++ index.js
@@ -18,12 +18,6 @@ app.use(cors({
     origin : 'http://localhost:3000',
     credentials : true
 }))
-  app.use((req,res,next) => {
-res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-next()
-})
 
 require('dotenv').config();
 const PORT = process.env.PORT;
