ba575cbf520ab3f342dedfb47a1676a61d0825f6
Coder-Arm <113359996+Coder-Arm@users.noreply.github.com>
2023-12-24 14:33:28 +0530

    changed origin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({extended : true}));
 app.use(cookieParser());
 
 app.use(cors({
-    origin : 'http://localhost:3000',
+    origin : 'https://codebox-1.netlify.app',
     credentials : true
 }))
 
